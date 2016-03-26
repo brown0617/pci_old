831c073b2ecfcc75a7e693fd1d428bef35b7bcaf
jeff.brown <jeffery.p.brown@outlook.com>
2016-03-26 18:59:41 -0400

    [Web] - added properties to customer detail view

---
+++ src/Web/app/services/data/propertyData.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+
+function propertyData($http) {
+	var uri = 'http://localhost:32150/api/properties';
+
+	//	fetch all properties
+	this.getAll = function() {
+		return $http.get(uri);
+	};
+
+	//	fetch a property by id
+	this.get = function (id) {
+		return $http.get(uri + '/' + id);
+	};
+
+	//	fetch a property by customer id
+	this.getByCustomerId = function (customerId) {
+		return $http.get(uri + '/customer/' + customerId);
+	};
+
+	//	fetch a new property
+	this.new = function() {
+		return $http.get(uri + '/new');
+	};
+
+	//	save a property
+	this.save = function(property) {
+		return $http.put(uri, property);
+	};
+};
+
+propertyData.$inject = ['$http'];
+app.service('propertyData', propertyData);
--- src/Web/app/views/crm/customer/customerDetailController.js
+++ src/Web/app/views/crm/customer/customerDetailController.js
@@ -1,13 +1,30 @@
 ﻿'use strict';
 
-function CustomerDtlCtrl(customerData, $stateParams, previousState) {
+function CustomerDtlCtrl($stateParams, previousState, $q, customerData, propertyData) {
 	var self = this;
 
-	self.customer = {};
+	var promises = [
+		customerData.get($stateParams.id),
+		propertyData.getByCustomerId($stateParams.id)
+	];
 
-	customerData.get($stateParams.id).then(function(result) {
-		self.customer = result.data;
+	$q.all(promises).then(function(results) {
+		self.customer = results[0].data;
+		self.properties = results[1].data;
 	});
+
+	self.gridProperties = {
+		data: 'customerDtl.properties',
+		multiSelect: false,
+		columnDefs: [
+			{ field: 'Name', displayName: 'Name' },
+			{ field: 'AddressStreet1', displayName: 'Street Address' },
+			{ field: 'AddressCity', displayName: 'City' },
+			{ field: 'AddressState', displayName: 'State' },
+			{ field: 'AddressZip', displayName: 'Zip' },
+			{ name: 'edit', displayName: '', cellTemplate: '<a class="btn" ui-sref="pci.customerDetail({id: row.entity.Id})"><i class="fa fa-pencil-square-o"></i></a>' }
+		]
+	};
 
 	this.cancel = function() {
 		return customerData.get($stateParams.id);
@@ -20,7 +37,7 @@ function CustomerDtlCtrl(customerData, $stateParams, previousState) {
 	this.previousState = previousState;
 }
 
-CustomerDtlCtrl.$inject = ['customerData', '$stateParams', 'previousState'];
+CustomerDtlCtrl.$inject = ['$stateParams', 'previousState', '$q', 'customerData', 'propertyData'];
 angular
 	.module('PCI')
 	.controller('CustomerDtlCtrl', CustomerDtlCtrl);
