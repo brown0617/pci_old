119f49c1019fc4e99c984da58dde712155b6a239
jeff.brown <jeffery.p.brown@outlook.com>
2016-04-03 17:04:57 -0400

    [Web] - changes to quoteDetail in UI

--- src/Web/app/views/quote/quoteDetailController.js
+++ src/Web/app/views/quote/quoteDetailController.js
@@ -7,12 +7,20 @@ function QuoteDtlCtrl($stateParams, $q, $filter, previousState, optionData, quot
 
 	var promises = [
 		quoteData.get($stateParams.id),
+		optionData.get('BillingDay'),
+		optionData.get('Month'),
+		optionData.get('QuoteStatus'),
+		optionData.get('QuoteType'),
 		optionData.get('Season')
 	];
 
 	$q.all(promises).then(function(results) {
 		self.quote = results[0].data;
-		self.seasons = results[1].data;
+		self.billingDays = results[1].data;
+		self.months = results[2].data;
+		self.statuses = results[3].data;
+		self.types = results[4].data;
+		self.seasons = results[5].data;
 	});
 
 	this.cancel = function() {
