d234d244535ef47442552a7d846bcdc61c87d50c
jeff.brown <jeffery.p.brown@outlook.com>
2016-04-09 15:28:43 -0400

    [Web] - work on quoteDetail and quoteItem

--- src/Web/app/views/quote/quoteDetailController.js
+++ src/Web/app/views/quote/quoteDetailController.js
@@ -1,9 +1,24 @@
 ﻿'use strict';
 
-function QuoteDtlCtrl($stateParams, $q, $filter, previousState, optionData, quoteData) {
+function QuoteDtlCtrl($stateParams, $q, $filter, $uibModal, previousState, optionData, quoteData) {
 	var self = this;
 
 	self.previousState = previousState;
+
+	function getModalInstance(item) {
+		return $uibModal.open({
+			templateUrl: '../views/quote/quoteItemModal.html',
+			backdrop: 'static',
+			size: 'lg',
+			controller: 'QuoteItemModalCtrl',
+			controllerAs: 'ctrl',
+			resolve: {
+				item: function () {
+					return item;
+				}
+			}
+		});
+	};
 
 	var promises = [
 		quoteData.get($stateParams.id),
@@ -16,12 +31,32 @@ function QuoteDtlCtrl($stateParams, $q, $filter, previousState, optionData, quot
 
 	$q.all(promises).then(function(results) {
 		self.quote = results[0].data;
+		self.quoteItems = $filter('orderBy')(self.quote.Items, 'Service.Name');
 		self.billingDays = results[1].data;
 		self.months = results[2].data;
 		self.statuses = results[3].data;
 		self.types = results[4].data;
 		self.seasons = results[5].data;
 	});
+
+	self.editItem = function (item) {
+		var modalInstance = getModalInstance(item);
+		modalInstance.result.then(function (updatedItem) {
+			// add contact
+			//personData.save(addedContact);
+		});
+	};
+
+	self.gridConfig = {
+		data: 'quoteDtl.quoteItems',
+		multiSelect: false,
+		columnDefs: [
+			{ field: 'Service.Name', displayName: 'Service', width: '20%' },
+			{ field: 'Description', displayName: 'Description' },
+			{ field: 'ServiceQuantity', displayName: 'Man Hours', width: '10%', cellClass: 'text-right' },
+			{ name: 'edit', displayName: '', cellTemplate: '<a class="btn" ng-click="grid.appScope.quoteDtl.editItem(row.entity)"><i class="fa fa-pencil-square-o"></i></a>', width: '5%' }
+		]
+	};
 
 	this.cancel = function() {
 		return quoteData.get($stateParams.id);
@@ -34,7 +69,8 @@ function QuoteDtlCtrl($stateParams, $q, $filter, previousState, optionData, quot
 	this.getPreviousState = function() {
 		return $q.when(self.previousState);
 	};
+
 }
 
-QuoteDtlCtrl.$inject = ['$stateParams', '$q', '$filter', 'previousState', 'optionData', 'quoteData'];
+QuoteDtlCtrl.$inject = ['$stateParams', '$q', '$filter', '$uibModal', 'previousState', 'optionData', 'quoteData'];
 app.controller('QuoteDtlCtrl', QuoteDtlCtrl);
---
+++ src/Web/app/views/quote/quoteItemModalCtrl.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+function QuoteItemModalCtrl($uibModalInstance, item) {
+	var self = this;
+	self.item = item;
+
+	this.save = function() {
+		$uibModalInstance.close(self.item);
+	};
+
+	this.cancel = function() {
+		$uibModalInstance.dismiss();
+	};
+};
+
+QuoteItemModalCtrl.$inject = ['$uibModalInstance', 'item'];
+app.controller('QuoteItemModalCtrl', QuoteItemModalCtrl);
